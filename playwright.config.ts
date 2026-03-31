011eff6f67706431d0393269ab09d8f1e305c172
Vojtěch Balcar <vojtech.balcar@sbid.cz>
2026-03-31 20:16:25 +0200

    chore: bootstrap keycroft app tooling

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import path from 'node:path'
+import { defineConfig } from 'playwright/test'
+
+export default defineConfig({
+  testDir: './e2e',
+  use: {
+    baseURL: 'http://127.0.0.1:3000',
+  },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium',
+      },
+    },
+  ],
+  outputDir: path.join('.playwright', 'artifacts'),
+})
